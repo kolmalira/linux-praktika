31e97ad25d01b4aee9a79396d3817a2f1606371a
DianaL <dianaluk2006@gmail.com>
2026-05-13 00:14:35 +0700

    ситуация 7. открывается документация по команде, ключ отдельно вынесен в файл

---
+++ sit7.sh
@@ -0,0 +1,7 @@
+echo "для просмотра документации используется команда man"
+echo "man grep"
+man grep
+echo ""
+echo " -i, --ignore-case Ignore case distinctions in patterns and input data, so that characters that differ only in case match each other."
+echo "для игнорирования регистра кoм grep используется ключ -r"
+
