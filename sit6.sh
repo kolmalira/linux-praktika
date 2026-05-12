aa9d6d15b437ed45303e912ebd8aebbcd542605f
DianaL <dianaluk2006@gmail.com>
2026-05-13 00:03:19 +0700

    ситуация 6. две команды с выводом. НЕ ЗАБЫТЬ СПРОСИТЬ ПО 2

---
+++ sit6.sh
@@ -0,0 +1,13 @@
+echo "команда"
+echo "apt-file list nginx"
+echo ""
+echo "вывод команды"
+apt-file list nginx
+echo ""
+echo "выводятся все файлы и пакеты nginx"
+echo ""
+echo  "команда для поиска конф файла"
+echo "sudo find /etc -name 'nginx.conf'"
+sudo find /etc -name "nginx.conf"
+
+echo "выводятся только конфиг файлы"
